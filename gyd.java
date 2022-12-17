c5277d18f6dd142d5f541d5deb96f7f1fb0293c1
名kf <109252832+13736775749@users.noreply.github.com>
2022-12-17 23:44:22 +0800

    Create gyd.java

---
+++ gyd.java
@@ -0,0 +1,12 @@
+public class gyd
+{
+public static void main(String[] args) {
+for(int i=1;i<=9;i++){
+for(int j=1;j<=i;j++){
+System.out.print(j+"×"+i+"="+i*j+" ");
+}
+System.out.println();
+}
+}
+}
+Footer
