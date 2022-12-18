585d95abd5bde2f1348232d31c9d1a17309dc52a
名kf <109252832+13736775749@users.noreply.github.com>
2022-12-18 19:21:28 +0800

    Create mian.java

---
+++ mian.java
@@ -0,0 +1,29 @@
+import java.net.*;
+import java.io.*; 
+class main
+ 
+{
+    public static void main(String args[]) throws Exception 
+	
+	{
+            try 
+		{
+            URL url = new URL("https://music.163.com/m/song?id=465921195&fromSimiSong=1");
+            InputStream in =url.openStream();
+            InputStreamReader isr = new InputStreamReader(in);
+            BufferedReader bufr = new BufferedReader(isr);
+            String str;
+            while ((str = bufr.readLine()) != null) 
+		{
+            System.out.println(str);
+        }
+            bufr.close();
+            isr.close();
+            in.close();
+        }
+		    catch (Exception e)
+		{
+            e.printStackTrace();
+        }
+      }
+	}
